258279267c4ccfede430f93b90b2650c13511912
Oliver Tale-Yazdi <oliver@perun.network>
2020-04-28 10:33:53 +0000

    :page_facing_up: [demo] Changed copyright headers

--- cmd/demo/payment.go
+++ cmd/demo/payment.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2019 Chair of Applied Cryptography, Technische Universität
-// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
-// of this source code is governed by a MIT-style license that can be found in
-// the LICENSE file.
+// Darmstadt, Germany. All rights reserved. This file is part of
+// perun-eth-demo. Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
 
 package demo
 
